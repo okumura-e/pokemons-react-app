2771c605827f2fddf9d9736c70abbb4f38268dee
Adriel Itamar <adrielitamar@gmail.com>
2023-10-08 12:06:54 -0300

    feat: consuming pokeapi

--- src/templates/first/index.jsx
+++ src/templates/first/index.jsx
@@ -1,27 +1,61 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Layout from "../../components/Layout";
 import "./styles.css";
 import Card from "../../components/Card";
+import api from "../../services/api";
 
 function App() {
   const [search, setSearch] = useState("");
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const getPokemons = async () => {
+      api
+        .get("/pokemon?limit=8")
+        .then((response) => {
+          setPokemons(response.data.results);
+        })
+        .catch((error) => {});
+    };
+    getPokemons();
+  }, []);
+
+  const handleInputChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    api
+      .get(`/pokemon/${search}`)
+      .then((response) => {
+        setPokemons([response.data]);
+      })
+      .catch((error) => {});
+    event.preventDefault();
+  };
 
   return (
     <Layout>
       <form>
-        <input type="text" placeholder="Pesquisar"/>
-        <button>Pesquisar</button>
+        <input
+          onChange={handleInputChange}
+          value={search}
+          type="text"
+          placeholder="Pesquisar"
+        />
+        <button type="submit" onClick={handleSubmit}>
+          Pesquisar
+        </button>
       </form>
 
       <section className="grid">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {pokemons?.map((pokemon, index) => (
+          <Card
+            key={index}
+            name={pokemon.name}
+            image={pokemon?.sprites?.other?.dream_world?.front_default}
+          />
+        ))}
       </section>
     </Layout>
   );
