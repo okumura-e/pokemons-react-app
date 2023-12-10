89f92f60c2fa543f6b2b8ccf69fe14ac02ca551b
Adriel Itamar <adrielitamar@gmail.com>
2023-12-10 14:43:44 -0300

    feat: add dinamic data from API

--- src/templates/second/index.jsx
+++ src/templates/second/index.jsx
@@ -1,24 +1,61 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
+import api from "../../services/api";
+import Layout from "../../components/Layout";
+import "./styles.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [pokemon, setPokemon] = useState();
+  const { name } = useParams();
+  
+  const getPokemons = async () => {
+    api
+      .get(`/pokemon/${name}`)
+      .then((response) => {
+        setPokemon(response.data);
+      })
+      .catch((error) => {});
+  };
+  useEffect(() => {
+    getPokemons();
+  }, []);
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Layout>
+      <img src={pokemon?.sprites?.other?.dream_world?.front_default} id="pokeIMG"/>
+      
+      <section id="title">
+        <h1>{name}</h1>
+      </section>
+
+      <section className="grid">
+        <section>
+          <h2>Abilites</h2>
+          {pokemon?.abilities.map((item, index) => (
+            <p key={index}>{item.ability.name}</p>
+          ))}
+        </section>
+
+        <section>
+          <h2>Types</h2>
+          {pokemon?.types.map((item, index) => (
+            <p key={index}>{item.type.name}</p>
+          ))}
+        </section>
+
+        <section>
+          <h2>Held items</h2>
+          {pokemon?.held_items.map((item, index) => (
+            <p key={index}>{item.item.name}</p>
+          ))}
+        </section>
+
+        <section>
+          <h2>Base experience</h2>
+          <p>{pokemon?.base_experience}</p>
+        </section>
+      </section>
+    </Layout>
   );
 }
 
